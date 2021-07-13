ed27122c204be8b6928666b7cfc9a7b0c68acf3e
SiposLevente <sipi.levente@gmail.com>
2021-07-13 19:34:56 +0200

    implemened data sending to shift register

--- ShiftRegisterController.ino
+++ ShiftRegisterController.ino
@@ -4,5 +4,18 @@
 
 void SendData(unsigned char data)
 {
-    
+    DDRB &= 0xF9;
+    for (int i = 0; i < sizeof(data); i++)
+    {
+        DDRB &= 0xFE;
+        if (data && (1 << i))
+        {
+            DDRB |= (1 << REG_DATAINPUT);
+        }
+        DDRB ^= (1 << REG_STORAGECLK);
+        DDRB ^= (1 << REG_STORAGECLK);
+    }
+
+    DDRB ^= (1 << REG_OUTPUTCLK);
+    DDRB ^= (1 << REG_OUTPUTCLK);
 }
