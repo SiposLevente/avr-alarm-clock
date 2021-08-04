77f3ba10ff547bb8d2ffdf2fe859c04b511abd37
SiposLevente <sipi.levente@gmail.com>
2021-08-04 22:36:37 +0200

    added digit caching method

--- TimeConverter.h
+++ TimeConverter.h
@@ -3,3 +3,5 @@
 // E.g.: (Time: 43200 [12:00]) -> TimeToNum(0) => output = 1
 // (Time: 43200 [12:00]) -> TimeToNum(1) => output = 2
 int TimeToNum(int digit, int time);
+
+void CacheDigits(unsigned int digitsCache[], unsigned int time);
