be0ea4df90ac68737e9f0eed8b3739426ccc6417
SiposLevente <sipi.levente@gmail.com>
2021-08-08 12:48:30 +0200

    added leap year indicator vairable

--- TimeConverter.h
+++ TimeConverter.h
@@ -7,15 +7,17 @@ int TimeToNum(int digit);
 // Caches the currently shown digits.
 void CacheDigits(unsigned int digitsCache[]);
 
-// Days in each month
+// Days in each month.
 unsigned int monthDayNum[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+// Indicates if the current year is a leap year.
+unsigned char isLeapYear = 0;
 
 // Holds the time value in minutes. Default value = 720 (12:00).
 int time = 720;
 
-// Stores the year
+// Stores the year.
 int year = 2021;
 
-// Stores the day and the month
+// Stores the day and the month.
 int date = 1;
