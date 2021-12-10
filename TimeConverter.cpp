25ead9607b6a7d49c75d409d9b5a7cc3c71be715
SiposLevente <sipi.levente@gmail.com>
2021-12-10 18:10:55 +0100

    Fixed time

--- TimeConverter.cpp
+++ TimeConverter.cpp
@@ -44,6 +44,34 @@ int TimeToNum(int digit)
     return returnDigit;
 }
 
+int TimeToNum(int time, int digit)
+{
+    int returnDigit = -1;
+    switch (digit)
+    {
+    case 0:
+        returnDigit = time / 600;
+        break;
+
+    case 1:
+        returnDigit = (time / 60) - TimeToNum(time, 0) * 10;
+        break;
+
+    case 2:
+        returnDigit = (time % 60) / 10;
+        break;
+
+    case 3:
+        returnDigit = (time % 60) - TimeToNum(time, 2) * 10;
+        break;
+
+    default:
+        break;
+    }
+
+    return returnDigit;
+}
+
 int DateToNum(int digit)
 {
     switch (digit)
