50d0a0eb7662213965c43767c87731e040b66811
SiposLevente <sipi.levente@gmail.com>
2021-12-10 18:15:13 +0100

    added alarm cache

--- headers/Alarm.h
+++ headers/Alarm.h
@@ -11,6 +11,9 @@ struct Alarm
 // Letters: A, L, r, E, n
 unsigned char digitLetters[5] = {0x77, 0x38, 0x50, 0x79, 0x54};
 
+// Stores displayed alarm in a numerical format.
+extern unsigned int alarmCache[4];
+
 // Stores the currently selected alarm.
 unsigned char currentAlarm = 0;
 
@@ -20,3 +23,7 @@ Alarm alarms[ALARMCOUNT];
 // Displays the alarms.
 // digitNum = number of displayed digit.
 void DisplayAlarms(int digitNum);
+
+// Displays the time of the currently selected alarm
+// digitNum = number of displayed digit.
+void DisplayAlarmTime(int digitNum);
